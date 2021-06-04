7e16c6646af11c2f9c1e294008d736861492b0c5
Kev <legui.kevin@gmail.com>
2021-06-04 19:21:11 -0300

    connect to dynamodb

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,31 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
 	const { title } = JSON.parse(event.body);
 	const now = new Date();
 
 	const auction = {
+		id: uuid(),
 		title,
 		status: 'OPEN',
 		createdAt: now.toISOString(),
 	};
+
+	try {
+		await dynamodb
+			.put({
+				TableName: 'AuctionsTable',
+				Item: auction,
+			})
+			.promise();
+	} catch (err) {
+		console.lo(err);
+		throw new createError.InternalServerError(err);
+	}
 
 	return {
 		statusCode: 201,
